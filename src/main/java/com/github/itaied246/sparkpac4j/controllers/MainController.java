c3c1a056f4e24d0d40218e79e4d51bb1be980217
Itai Edri <itaied246@gmail.com>
2017-01-06 13:40:44 +0200

    Get the client name from the response

--- src/main/java/com/github/itaied246/sparkpac4j/controllers/MainController.java
+++ src/main/java/com/github/itaied246/sparkpac4j/controllers/MainController.java
@@ -1,5 +1,12 @@
 package com.github.itaied246.sparkpac4j.controllers;
 
+import java.util.Optional;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.sparkjava.SparkWebContext;
+
 import spark.Request;
 import spark.Response;
 
@@ -10,7 +17,17 @@ public class MainController {
 	}
 
 	public String authHello(Request req, Response res) {
-		return "authenticated hello world!";
+		String clientName = getClientName(req, res);
+		return "Hello " + clientName;
+	}
+
+	private String getClientName(Request req, Response res) {
+		WebContext context = new SparkWebContext(req, res);
+		ProfileManager<CommonProfile> manager = new ProfileManager<CommonProfile>(
+				context);
+		Optional<CommonProfile> profile = manager.get(true);
+		return profile.map(commonProfile -> commonProfile.getClientName())
+				.orElse("Unknown");
 	}
 
 }
